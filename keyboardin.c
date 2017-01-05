898f5fc1459a40fd9202b6e0aaeac040835d3a22
JoshuaReavis <joshuareavis108@gmail.com>
2017-01-05 15:56:00 -0800

    Update keyboardin.c

--- keyboardin.c
+++ keyboardin.c
@@ -1,6 +1,6 @@
 // Josh Reavis
 // Summer 2016
-// CSCI 352
+// Created for CSCI 352 Unix Software Development
 
 // This program enhances the command line interpreter with dynamic command history capability.
 // Used in conjuction with the history array passed in through CLI.c, betterfgets allows us to
